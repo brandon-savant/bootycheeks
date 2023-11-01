be9ce4a6adb6d832f25a04ea3767c14fd1315892
brandon-savant <brandon@savantcts.com>
2023-11-01 13:12:47 -0500

    added attempt at free space on c

--- server report.ps1
+++ server report.ps1
@@ -6,10 +6,14 @@ $ComputerInfo = Get-ComputerInfo
 $DicksInfo = Get-Disk
 #$DicksInfo.count 
 
+# get free system space
+$FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
+
 $obj = [PSCustomObject]@{
     ComputerName = $ComputerInfo.CsDNSHostName
     OSVersion = $ComputerInfo.OsVersion
     LogicalDicks = $DicksInfo.count
+    FreeSpace = $FreeSpace.Free
 }
 Write-Output $obj
 
