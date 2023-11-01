9b570af9602b62dae9b1f72eed0a3a16561ccb1c
brandon-savant <brandon@savantcts.com>
2023-11-01 14:04:36 -0500

    added cores

--- server report.ps1
+++ server report.ps1
@@ -9,11 +9,15 @@ $DicksInfo = Get-Disk
 # get free system space
 $FreeSpace = Get-PSDrive | Where-Object {$_.Name -eq 'C'}
 
+# get number of cores
+$CPUCore = Get-CimInstance CIM_Processor
+
 $obj = [PSCustomObject]@{
     ComputerName = $ComputerInfo.CsDNSHostName
     OSVersion = $ComputerInfo.OsVersion
     LogicalDicks = $DicksInfo.count
     FreeSpace_GB = [System.Math]::Round(($FreeSpace.Free / 1gb),0)
+    CPUCores = $CPUCore.NumberofCores
 
 }
 Write-Output $obj
